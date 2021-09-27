bace0f247014e9b8ef3605c89c43088a25b93234
ThugRaven <kamilo1715@wp.pl>
2021-09-28 00:45:27 +0200

    Add functions

--- script.js
+++ script.js
@@ -2,11 +2,31 @@ import { API_KEY } from "./settings.js";
 import Weather from "./weather.js";
 
 const weather = new Weather(API_KEY);
+// weather.setUnits("standard");
 
+const locationInput = document.querySelector("[data-input]");
 const weatherButton = document.querySelector("[data-button]");
+const weatherCity = document.querySelector(".weather__city");
+const weatherIcon = document.querySelector("[data-icon]");
 
-weatherButton.addEventListener("click", () => {
-	weather.getCurrentWeather("Kraków").then((weather) => {
-		console.log(weather);
-	});
+weatherButton.addEventListener("click", (event) => {
+	event.preventDefault();
+	const location = locationInput.value.trim();
+	if (location == null || location === "") return;
+
+	weather
+		.getCurrentByCityName(location)
+		.then((current) => {
+			console.log(current);
+
+			weatherCity.innerHTML = `${current.sys.country}, ${current.name}<br />${
+				current.main.temp
+			} &deg;C | ${weather.celsiusToFahrenheit(current.main.temp)} &deg;F`;
+			weatherIcon.src = weather.getWeatherIcon(current.weather[0].icon);
+		})
+		.catch((err) => {
+			console.error(err);
+
+			weatherCity.innerHTML = "City not found";
+		});
 });
--- weather.js
+++ weather.js
@@ -22,9 +22,7 @@ export default class Weather {
 		return this.LANGUAGE;
 	}
 
-	getCurrentWeather(cityName) {
-		let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.API_KEY}`;
-
+	prepareURLFetch(url) {
 		if (this.UNITS) {
 			url += `&units=${this.UNITS}`;
 		}
@@ -33,10 +31,45 @@ export default class Weather {
 			url += `&lang=${this.LANGUAGE}`;
 		}
 
-		return fetch(url)
-			.then((res) => res.json())
-			.then((data) => {
-				return data;
-			});
+		return fetch(url).then((res) => {
+			if (res.ok) {
+				return res.json();
+			} else {
+				throw new Error(res.status);
+			}
+		});
+	}
+
+	getCurrentByCityName(cityName) {
+		let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+			cityName
+		)}&appid=${this.API_KEY}`;
+
+		return this.prepareURLFetch(url);
+	}
+
+	getCurrentByCityID(cityID) {
+		let url = `https://api.openweathermap.org/data/2.5/weather?id=${encodeURI(
+			cityID
+		)}&appid=${this.API_KEY}`;
+
+		return this.prepareURLFetch(url);
+	}
+
+	getCurrentByLatLong(lat, long) {
+		let url = `https://api.openweathermap.org/data/2.5/weather?lat=${encodeURI(
+			lat
+		)}&lon=${encodeURI(long)}&appid=${this.API_KEY}`;
+
+		return this.prepareURLFetch(url);
+	}
+
+	celsiusToFahrenheit(temp) {
+		return temp * (9 / 5) + 32;
+	}
+
+	getWeatherIcon(id) {
+		let url = `https://openweathermap.org/img/wn/${id}@2x.png`;
+		return url;
 	}
 }
